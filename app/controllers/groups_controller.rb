c9df326ffe3436aad6fab010597f3beedd6b629f
huangpuxiang <hpx0809@outlook.com>
2017-11-09 11:45:21 +0800

    only group creater can edit,delete group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update, :new, :create, :destroy]
+  before_action :find_group_and_check_permisson, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -8,17 +10,15 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to groups_path
       flash[:notice] = "群组更新成功"
     else
       render :edit
-    end  
+    end
   end
 
   def new
@@ -27,6 +27,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.user = current_user
     if @group.save
       redirect_to groups_path
     else
@@ -35,12 +36,19 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
     @group.destroy
     redirect_to groups_path, alert: "删除成功！"
   end
 
   private
+
+  def find_group_and_check_permisson
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to groups_path, alert: "you have no permission"
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
